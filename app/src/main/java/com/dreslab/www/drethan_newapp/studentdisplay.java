7a637a7fed5a8082f2fbd5357ca25f51b3a7dac9
Dan Callahan <dancallahan05@gmail.com>
2017-04-11 22:11:40 -0400

    removed invalid images

--- app/src/main/java/com/dreslab/www/drethan_newapp/studentdisplay.java
+++ app/src/main/java/com/dreslab/www/drethan_newapp/studentdisplay.java
@@ -1,6 +1,7 @@
 package com.dreslab.www.drethan_newapp;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 import android.content.Context;
@@ -13,7 +14,7 @@ import android.widget.ImageView;
  * Created by DanCallahan on 4/11/17.
  */
 
-public class studentdisplay {
+public class studentdisplay extends AppCompatActivity{
 
     ImageView img_plate;
     ImageView img_fork;
@@ -25,18 +26,26 @@ public class studentdisplay {
     Button btn_spoon;
     Button btn_knife;
 
-    /*
+
     protected void onCreate(Bundle savedInstanceBundle){
 
-        img_plate = (ImageView) studentdisplay.findViewById(R.id.plate);
+        img_plate = (ImageView) findViewById(R.id.plate);
         img_fork = (ImageView) findViewById(R.id.fork);
         img_knife = (ImageView) findViewById(R.id.knife);
         img_spoon = (ImageView) findViewById(R.id.spoon);
 
+        btn_plate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                img_plate.setVisibility(View.INVISIBLE);
+            }
+
+
+        });
 
     }
 
-*/
+
 
 
 }
