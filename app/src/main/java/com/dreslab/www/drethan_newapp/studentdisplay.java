ce124174e6e109db3669cb9baff8d402c253afca
Dan Callahan <dancallahan05@gmail.com>
2017-04-11 21:50:27 -0400

    trying out java files

---
+++ app/src/main/java/com/dreslab/www/drethan_newapp/studentdisplay.java
@@ -0,0 +1,45 @@
+package com.dreslab.www.drethan_newapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+/**
+ * Created by DanCallahan on 4/11/17.
+ */
+
+public class studentdisplay {
+
+    ImageView img_plate;
+    ImageView img_fork;
+    ImageView img_knife;
+    ImageView img_spoon;
+
+    Button btn_plate;
+    Button btn_fork;
+    Button btn_spoon;
+    Button btn_knife;
+
+    /*
+    protected void onCreate(Bundle savedInstanceBundle){
+
+        img_plate = (ImageView) studentdisplay.findViewById(R.id.plate);
+        img_fork = (ImageView) findViewById(R.id.fork);
+        img_knife = (ImageView) findViewById(R.id.knife);
+        img_spoon = (ImageView) findViewById(R.id.spoon);
+
+
+    }
+
+*/
+
+
+}
+
+
+
