6c9e8ef223297804d53b67c4015734074ec0a785
Dan Callahan <dancallahan05@gmail.com>
2017-04-25 11:42:29 -0400

    removed buttons from student display

---
+++ app/src/main/java/com/dreslab/www/drethan_newapp/teacherctrl.java
@@ -0,0 +1,13 @@
+package com.dreslab.www.drethan_newapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by DanCallahan on 4/25/17.
+ */
+
+public class teacherctrl extends AppCompatActivity implements View.OnClickListener{
+
+    
+}
